18cfcc805307a9e5e4f655e109d78dd353f316bd
岡野雄介 <tkiptkn@gmail.com>
2021-05-24 12:11:04 +0900

    返送用のボックスの案内ページ追加

---
+++ page-return-box.php
@@ -0,0 +1,190 @@
+<?php
+/*
+Template Name: 返送用ボックスについて
+*/
+?>
+<?php get_header();?>
+
+
+
+
+
+  <div class="p-pages-ttl-area --ttl-bg-items u-bg-cover" id="js-pages-head">
+        <div class="p-pages-ttl-area__inner --inner-item">
+              <div class="p-pages-ttl-area__texts --texts--item-list u-sp-tc">
+                        <h1 class="p-pages-ttl-area__ttl --ttl--item-list">ご返送方法変更のご案内</h1>
+                <p class="p-pages-ttl-area__sub">ご返送用封筒の郵便取扱終了について</p>
+              </div>
+        </div>
+</div>
+
+
+<div class="c-breadcrumbs" typeof="BreadcrumbList" vocab="https://schema.org/">
+    <?php if(function_exists('bcn_display'))
+    {
+        bcn_display();
+    }?>
+</div>
+
+
+<section class="p-return u-pc">
+
+
+    <div class="l-inner p-return__inner">
+
+            <ul class="p-leaflet">
+                <li class="p-leaflet__item">
+                        <dl class="p-leaflet__list">
+                           <!--  <dt class="p-leaflet__name">表</dt> -->
+                            <dd class="p-leaflet__img">
+                                <a href="<?php echo home_url( '/' ); ?>return-box">
+<img src="<?php echo get_template_directory_uri(); ?>/img/leaflet/leaflet01.jpg?045522266" alt=""
+                                class="js-img-scale"></a>
+                            </dd>
+                        </dl>
+                </li>
+                <li class="p-leaflet__item">
+                        <dl class="p-leaflet__list">
+                           <!--  <dt class="p-leaflet__name">裏</dt> -->
+                            <!-- <dd class="p-leaflet__img">
+                                <img src="<?php echo get_template_directory_uri(); ?>/img/leaflet/leaflet02.jpg?045522266" alt=""
+                                class="js-img-scale">
+                            </dd> -->
+                        </dl>
+                </li>
+            </ul>
+  </div>
+</section>
+
+
+        <section class="u-sp p-sp-area">
+
+            <div class="l-inner">
+
+
+                <h2 class="p-rt-sp-ttl">ご返送用封筒の郵便取扱終了について</h2>
+
+
+
+                <p class="p-rt-sp-desc">
+                <span style="color:blue;font-weight: bold;">5/24までに検査キットをご購入いただいた方で、返送用封筒をお持ちの方は返送用ボックスをご注文ください。無償にて提供させていただいております。</span><br><br>
+
+従来、封筒を用いて検体のご返送をご案内しておりましたが、日本郵便様からの通達により、2021年5月以降より、世界保健機関(WHO)の定めるガイドラインに準拠した内容へと変更となりました。<br class="">これに伴い、従来の封筒を用いてのご返送は出来ませんのでご注意ください。<br class="">ご返送方法変更に伴い、多大なご迷惑をお掛け致しました事、深くお詫び申し上げると共に、何卒ご理解賜ります様お願い申し上げます。</p>
+
+
+                <ul class="p-sp-row">
+                        <li class="p-sp-row__list">
+                            <img src="<?php echo get_template_directory_uri(); ?>/img/leaflet/col01.png" alt="">
+                        </li>
+
+                        <li class="p-sp-row__tt">
+                            <dl class="p-sp-row__data u-tc c-red">
+                                <dt class="p-sp-row__name u-tc ufw600">＜注意＞</dt>
+                                <dd class="p-sp-row__desc">返送用封筒にてポストへ投函されますと、郵便物として、お取り扱いが出来ず、当院まで検体が送付されませんのでご注意ください。</dd>
+                            </dl>
+                        </li>
+                </ul>
+                </div>
+
+<a href="<?php echo home_url( '/' ); ?>return-box">
+<img src="<?php echo get_template_directory_uri(); ?>/img/leaflet/box_thumb_btn.jpg?045522266" alt="">
+</a>
+
+
+           <!-- <div class="bg-section-gray p-set">
+                <div class="l-inner">
+                    <h2 class="c-title-under-circle p-set__ttl c-blue">返送用ボックス組み立てについて</h2>
+                    <ul class="p-set-list u-flex u-flex-wrap">
+                        <li class="p-set-list__item"><img src="<?php echo get_template_directory_uri(); ?>/img/leaflet/box_thumb01.png" alt=""></li>
+                        <li class="p-set-list__item"><img src="<?php echo get_template_directory_uri(); ?>/img/leaflet/box_thumb02.png" alt=""></li>
+                        <li class="p-set-list__item"><img src="<?php echo get_template_directory_uri(); ?>/img/leaflet/box_thumb03.png" alt=""></li>
+                        <li class="p-set-list__item"><img src="<?php echo get_template_directory_uri(); ?>/img/leaflet/box_thumb04.png" alt=""></li>
+                    </ul>
+                    <p class="p-set-midl">
+                        <img src="<?php echo get_template_directory_uri(); ?>/img/leaflet/box_desc.png" alt="">
+                    </p>
+
+                     <ul class="p-set-list u-flex u-flex-wrap">
+                        <li class="p-set-list__item"><img src="<?php echo get_template_directory_uri(); ?>/img/leaflet/box_thumb05.png" alt=""></li>
+                        <li class="p-set-list__item"><img src="<?php echo get_template_directory_uri(); ?>/img/leaflet/box_thumb06.png" alt=""></li>
+                    </ul>
+
+                </div>
+            </div>-->
+
+            <!-- <div class="p-set02">
+
+                <div class="l-inner">
+                    <h2 class="c-title-under-circle p-set02__ttl c-blue">検体の梱包・ご返送について</h2>
+                    <ul class="p-set02-list">
+                            <li class="p-set02-list__ttl">
+                                <img src="<?php echo get_template_directory_uri(); ?>/img/leaflet/box02_thumb01.jpg" alt="">
+                            </li>
+                            <li class="p-set02-list__ttl">
+                                <img src="<?php echo get_template_directory_uri(); ?>/img/leaflet/box02_thumb02.jpg" alt="">
+                            </li>
+
+                            <li class="p-set02-list__ttl">
+                                <img src="<?php echo get_template_directory_uri(); ?>/img/leaflet/box02_thumb03.jpg" alt="">
+                            </li>
+
+                            <li class="p-set02-list__ttl">
+                                <img src="<?php echo get_template_directory_uri(); ?>/img/leaflet/box02_thumb05.jpg" alt="">
+                            </li>
+                            <li class="p-set02-list__ttl">
+                                <img src="<?php echo get_template_directory_uri(); ?>/img/leaflet/box02_thumb06.jpg" alt="">
+                            </li>
+                    </ul>
+                </div>
+            </div>-->
+
+              </section>
+
+
+            <!--<a href="<?php echo home_url( '/' ); ?>return-box" class="p-btn-404 c-btn p-return__btn">注文ページに進む</a>-->
+
+
+
+
+
+
+
+<!-- モーダル -->
+
+    <div class="c-modal js-modal" id="js-img-detail">
+      <div class="c-modal__bg js-modal-close"></div>
+      <div class="c-modal__inner" id="js-img-content">
+        <div class="">
+            <img src="" alt="" id="js-leaf-src">
+        </div>
+        <p class="js-modal-close c-modal__btn u-mb0">閉じる</p>
+      </div>
+      <!-- ./c-modal__inner -->
+    </div>
+
+<!--
+<script>
+
+        const elmLeaflet = document.getElementsByClassName('js-img-scale');
+        const modal = document.getElementById('js-img-detail');
+        const modalContent = document.getElementById('js-img-content');
+        const body = document.getElementsByTagName('body');
+        const outImage = document.getElementById('js-leaf-src');
+
+        for(let i = 0; i < elmLeaflet.length; i++){
+                elmLeaflet[i].addEventListener('click',function(){
+                    var imgSrc = this.getAttribute("src");
+                    modalContent.style.width = "auto";
+                     modalContent.style.maxWidth = "90%";
+                    modal.classList.add("is-modal-open");
+                    body[0].classList.add("is-of-hidden");
+                    outImage.setAttribute("src",imgSrc);
+
+
+                });
+        }
+
+</script> -->
+
+
+<?php get_footer();?>
